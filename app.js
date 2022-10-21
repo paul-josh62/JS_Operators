160cc4097a71dabf1b56c259424d1acc02e12426
Joshua Paul <paul.josh62@gmail.com>
2022-10-21 08:50:44 -0500

    complete

--- app.js
+++ app.js
@@ -3,17 +3,78 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
+console.log("EXERCISE 1:\n==========\n")
+
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(multiply);
 
 // YOUR CODE HERE
 
 // Exercise 2
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+add = str + num;
+add = num + str2;
+add = num + isPresent;
+add = firstName + num;
+add = isPresent + str;
+add = firstName + lastName;
+
+
+//What is the value of: num + str? 1111
+//What is the value of: num + str2? 11eleven
+//What is the value of: num + isPresent? 12
+//What is the value of: firstName + num? Frodo11
+//What is the value of: isPresent + str? true11
+//What is the value of: firstName + lastName? FrodoBaggins
+
 console.log("EXERCISE 2:\n==========\n");
+
+
+console.log(add)
 
 // YOUR CODE HERE
 
 // Exercise 3
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+val == str3;
+val === str3;
+!isPresent2;
+"eleven" == str4 && val >= str3;
+!isPresent2 || isPresent2;
+0 == false;
+0 === false;
+0 != false;
+0 !== false;
+
+
+//What is the value of: val == str3? true
+//What is the value of: val === str3? false
+//What is the value of: !isPresent2? true
+//What is the value of: (“eleven” == str4 && val >= str3)? false 
+//What is the value of: (!isPresent2 || isPresent2)? true 
+//What is the value of: 0 == false? true
+//What is the value of: 0 === false? false 
+//What is the value of: 0 != false? false 
+//What is the value of 0 !== false? true
+
 console.log("EXERCISE 3:\n==========\n");
+console.log(0 !== false)
 
 // YOUR CODE HERE
